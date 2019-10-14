0d509d75975302ad174c77d3278aa8a2cae9d039
alexandriamw <alexandriawagner84@gmail.com>
2019-10-13 21:32:18 -0500

    deleted Groant component, wired up clicking behavior

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Groant from './components/Groant';
 
-function App() {
-  const Groants = [
-    <Groant photoId={"07"} alt={"a photo of Groant"} />,
-    <Groant photoId={"02"} alt={"a photo of Groant"} />,
-    <Groant photoId={"03"} alt={"a photo of Groant"} />,
-    <Groant photoId={"04"} alt={"a photo of Groant"} />,
-    <Groant photoId={"05"} alt={"a photo of Groant"} />,
-    <Groant photoId={"06"} alt={"a photo of Groant"} />,
-    <Groant photoId={"08"} alt={"a photo of Groant"} />,
-    <Groant photoId={"09"} alt={"a photo of Groant"} />,
-    <Groant photoId={"10"} alt={"a photo of Groant"} />,
-    <Groant photoId={"11"} alt={"a photo of Groant"} />,
-    <Groant photoId={"12"} alt={"a photo of Groant"} />,
-    <Groant photoId={"01"} alt={"a photo of Groant"} />
-  ];
+class App extends Component {
+  constructor (props) {
+    super(props);
 
-  return (
-    <div className="App">
-      <header>
-        <h1>Kitty Game!</h1>
-        <h2>Click on the kitty to earn points, but don't click on any image more than once!</h2>
-        <h3>Score: <span>0</span></h3>
-      </header>
-      <div className="grid-container">
-        {Groants}
+    this.groantClick = this.groantClick.bind(this);
+  }
+
+  groantClick (id) {
+    console.dir(id);
+  }
+  
+  render () {
+    return (
+      <div className="App">
+        <header>
+          <h1>Kitty Game!</h1>
+          <h2>Click on the kitty to earn points, but don't click on any image more than once!</h2>
+          <h3>Score: <span>0</span></h3>
+        </header>
+        <div className="grid-container">
+          <div className="grid-item"><img src="images/groant_01.jpg" alt="a weird Groant" onClick={this.groantClick.bind(this, 1)} /></div>
+          <div className="grid-item"><img src="images/groant_02.jpg" alt="an oldtimey Groant" onClick={this.groantClick.bind(this, 2)} /></div>
+          <div className="grid-item"><img src="images/groant_03.jpg" alt="an adorable Groant" onClick={this.groantClick.bind(this, 3)} /></div>
+          <div className="grid-item"><img src="images/groant_04.jpg" alt="a cute Groant" onClick={this.groantClick.bind(this, 4)} /></div>
+          <div className="grid-item"><img src="images/groant_05.jpg" alt="a hambeast Groant" onClick={this.groantClick.bind(this, 5)} /></div>
+          <div className="grid-item"><img src="images/groant_06.jpg" alt="a Groant ready to rock you like a hurricane" onClick={this.groantClick.bind(this, 6)} /></div>
+          <div className="grid-item"><img src="images/groant_07.jpg" alt="a snuggle Groant" onClick={this.groantClick.bind(this, 7)} /></div>
+          <div className="grid-item"><img src="images/groant_08.jpg" alt="a tarty Groant" onClick={this.groantClick.bind(this, 8)} /></div>
+          <div className="grid-item"><img src="images/groant_09.jpg" alt="a curious Groant" onClick={this.groantClick.bind(this, 9)} /></div>
+          <div className="grid-item"><img src="images/groant_10.jpg" alt="a Christmas Groant" onClick={this.groantClick.bind(this, 10)} /></div>
+          <div className="grid-item"><img src="images/groant_11.jpg" alt="a warm Groant" onClick={this.groantClick.bind(this, 11)} /></div>
+          <div className="grid-item"><img src="images/groant_12.jpg" alt="the best Groant" onClick={this.groantClick.bind(this, 12)} /></div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
