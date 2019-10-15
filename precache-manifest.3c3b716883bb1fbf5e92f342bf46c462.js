4fa8697c7d5c5f5c42a1de994cfe2b804f6dd4b7
alexandriamw <alexandriawagner84@gmail.com>
2019-10-15 18:14:34 -0500

    Updates

---
+++ precache-manifest.3c3b716883bb1fbf5e92f342bf46c462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c30fc6193f5c121165c4072408bb133",
+    "url": "/clicky-app/index.html"
+  },
+  {
+    "revision": "65c72c755b0682c54f7e",
+    "url": "/clicky-app/static/css/main.03438272.chunk.css"
+  },
+  {
+    "revision": "e917f102ae1659fb2be5",
+    "url": "/clicky-app/static/js/2.3fdc5ac3.chunk.js"
+  },
+  {
+    "revision": "65c72c755b0682c54f7e",
+    "url": "/clicky-app/static/js/main.1156afa4.chunk.js"
+  },
+  {
+    "revision": "6a1978139608be099078",
+    "url": "/clicky-app/static/js/runtime-main.f4ef3a77.js"
+  }
+]);
